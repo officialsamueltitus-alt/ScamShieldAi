b03664a91074c2434c2b572ab35e6e4880737f21
Titus <officialsamueltitus@gmail.com>
2026-02-26 08:02:55 +0100

    "Add error logging"

--- server.ts
+++ server.ts
@@ -1,3 +1,13 @@
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED REJECTION:', err);
+  process.exit(1);
+});
+
 import express from "express";
 import { createServer as createViteServer } from "vite";
 import path from "path";
